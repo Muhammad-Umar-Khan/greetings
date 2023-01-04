b630bae599ef4dec7deb4fbd0cdb8b6271ddc2a3
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-01-04 18:15:19 +0500

    merging again

--- move/greet.js
+++ move/greet.js
@@ -5,3 +5,11 @@ document.getElementById("name");
 
 console.log("This line shoud create merge conflict");
 console.log("This is after resting greet to the first merge command");
+
+console.log('Umar');
+console.log('Usman');
+console.log('Hamad');
+document.getElementById("name");
+
+console.log("This line shoud create merge conflict");
+console.log("This is after resting greet to the first merge command");
