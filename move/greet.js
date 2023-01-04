9dbbab7f34f867cb25f68fc55211f996cfc5c039
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-01-04 13:33:24 +0500

    added askName function

--- move/greet.js
+++ move/greet.js
@@ -5,3 +5,7 @@ console.log(greet("Umar"));
 const sayHello = (user_name) => `How are you ${user_name}`;
 
 console.log(sayHello("Umar"));
+
+const askName = () => `How are you?`;
+
+console.log(askName());
