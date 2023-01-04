1f832bac253dc3f249056b840e9aeeeb209053d7
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-01-04 18:25:33 +0500

    removed lines from greet on greet branch

--- move/greet.js
+++ move/greet.js
@@ -4,12 +4,3 @@ console.log('Hamad');
 document.getElementById("name");
 
 console.log("This line shoud create merge conflict");
-console.log("This is after resting greet to the first merge command");
-
-console.log('Umar');
-console.log('Usman');
-console.log('Hamad');
-document.getElementById("name");
-
-console.log("This line shoud create merge conflict");
-console.log("This is after resting greet to the first merge command");
