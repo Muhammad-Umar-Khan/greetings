0a037ee70544b4bc5438b5f64bd66da3feb688b0
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-01-04 18:08:17 +0500

    Revert "Move folder added"
    This reverts commit 0d3896d2879a577775c10c5278baa7e45e1d3950.

--- move/greet.js
+++ move/greet.js
@@ -4,4 +4,3 @@ console.log('Hamad');
 document.getElementById("name");
 
 console.log("This line shoud create merge conflict");
-console.log("This is after resting greet to the first merge command");
